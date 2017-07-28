7c7cefdc08f5f4b440be8cd03ed716805fe7637b
Anders Roxell <anders.roxell@gmail.com>
2017-07-28 07:12:38 +0200

    add Xdefaults.xps13 to override Xdefaults
    Signed-off-by: Anders Roxell <anders.roxell@gmail.com>

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ install_files=(
     "$HOME/.xinitrc:${reporoot}/xinitrc"
     "$HOME/.Xdefaults:${reporoot}/Xdefaults"
     "$HOME/.Xdefaults.zenbook:${reporoot}/Xdefaults.zenbook"
+    "$HOME/.Xdefaults.xps13:${reporoot}/Xdefaults.xps13"
     "$HOME/.Xsession:${reporoot}/xinitrc"
     "$HOME/.urxvt:${reporoot}/urxvt"
     "$HOME/.i3:${reporoot}/i3"
